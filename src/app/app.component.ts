a1becc67ceba3bee90e83e8d6ee779bfe862e8ed
John Paul Della Rosa <johnpauldellarosa@Johns-Mac-mini.fritz.box>
2023-03-17 23:09:57 +0100

    implementation of main game views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular--ssp_jpdr';
+export class AppComponent implements OnInit {
+
+  title: string = "angular--ssp_jpdr"
+
+  player = {
+    enemy: "ENEMY Player",
+    user: "YOU"
+  }
+
+  availablePlays = {
+    PAPER: "assets/svg/paper.svg",
+    SCISSORS: "assets/svg/scissors.svg",
+    ROCK: "assets/svg/rock.svg"
+  }
+
+  startGameData: any
+  startGame: boolean = false
+  showInstructions: boolean = true
+
+  enemyMove: string = " "
+  enemyMoveImage?: any
+  resultMessage: string = " "
+  playerMoveImage?: any
+
+  constructor(private http: HttpClient){}
+  
+  ngOnInit(): void {
+    this.http.get("http://localhost:8080/game/start")
+      .subscribe(
+        responseData => this.startGameData = responseData
+      )
+  }
+
+  onStartGame() {
+    this.startGame = true
+    this.showInstructions = false
+  }
+
+  setResultData(resultData: any) {
+    this.enemyMove = resultData.enemyOption
+    this.resultMessage = resultData.gameEndOption
+    this.enemyMoveImage = this.availablePlays[resultData.enemyOption as keyof typeof this.availablePlays]
+  }
+
+  setPlayerMoveImage(playerMove: any) {
+    this.playerMoveImage = this.availablePlays[playerMove as keyof typeof this.availablePlays]
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MoveOptionsComponent } from './move-options/move-options.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoveOptionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
